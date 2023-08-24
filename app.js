0b23b97199e4153547929a0a26f648935bd37888
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-08-24 07:38:53 -0500

    refactor: :recycle: Servidor
    Se levanta el servidor y se enrutan
    
    by: Jhon Hernandez

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { MyServer } from './config/variables.js';
+
+import appAnimales from './routers/animales.js';
+import appEmpleados from './routers/empleados.js';
+import appInsumos from './routers/insumos.js';
+import appTaquillas from './routers/taquilla.js';
+import appZonas from './routers/zonas.js';
+
+let app = express();
+app.use(express.json());
+
+app.use("/animales", appAnimales);
+app.use("/empleados", appEmpleados);
+app.use("/insumos", appInsumos);
+app.use("/taquillas", appTaquillas);
+app.use("/zonas", appZonas);
+
+app.listen(MyServer, () => console.log(`http://${MyServer.hostname}:${MyServer.port}`));
