a3421264ac4b3b26bcfce14cf44ab4beae708a1d
Nirclaw <caicedonicolas150@gmail.com>
2023-08-29 17:15:03 -0500

    feat: :sparkles: Se agrega nueva variable de entorno

--- config/variables.js
+++ config/variables.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import { con } from "../conexion/conexion.js";
 
 dotenv.config();
 
@@ -7,3 +8,4 @@ export const Apass = process.env.ATLAS_PASS;
 export const Adb = process.env.ATLAS_DB;
 export const MyServer = JSON.parse(process.env.SERVER)
 export const PASSWORD = process.env.PASSWORD
+export const db = await con()
