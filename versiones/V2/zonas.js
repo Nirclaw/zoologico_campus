9099954954d3ff7180698f31a84ea38a6f8c134a
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-08-29 22:30:21 -0500

    feat: :sparkles: Version 2 Zonas
    Se crean las funciones de POST PUT Y DELETE de la coleccion de zonas
    
    by: Jhon Hernandez

---
+++ versiones/V2/zonas.js
@@ -0,0 +1,64 @@
+import { db } from "../../config/variables.js";
+import { autoIncrement } from "../../helpers/autoincrement.js";
+
+let zonas = await db.collection("zonas");
+
+/*
+    {
+        "nombre": "Zona",
+        "clasificacion": "Clasificación C - Bajo riesgo",
+        "descripcion": "Zona",
+        "dimension": {
+            "ancho": 100,
+            "largo": 70
+        }
+    }
+*/
+export const zonasPost = async (req, res) => {
+    try {
+        let newID = await autoIncrement("zonas")
+
+        let data = await zonas.insertOne({
+            id: newID,
+            ...req.body
+        })
+            
+        res.status(200).send({status: 200, message: "Registro creado con exito"});
+
+    } catch (error) {
+      res.status(400).send({ status: 400, message: error });
+    }
+};
+
+export const zonasPut = async (req, res) => {
+    try {
+        let id = req.params.id
+        id = parseInt(id)
+
+        let data = await zonas.updateOne(
+            { id: id },
+            { $set: req.body }
+        )
+            
+        res.status(200).send({status: 200, message: "Registro actualizado con exito"});
+
+    } catch (error) {
+      res.status(400).send({ status: 400, message: error });
+    }
+};
+
+export const zonasDelete = async (req, res) => {
+    try {
+        let id = req.params.id
+        id = parseInt(id)
+
+        let data = await zonas.deleteOne(
+            { id: id },
+        )
+            
+        res.status(200).send({status: 200, message: "Registro eliminado con exito"});
+
+    } catch (error) {
+      res.status(400).send({ status: 400, message: error });
+    }
+};
