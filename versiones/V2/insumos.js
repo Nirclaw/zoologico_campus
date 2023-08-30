c3f4fc13837a9765809be4b323b321741d29cdea
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-08-29 22:00:02 -0500

    feat: :sparkles: Version 2 Insumos
    Se crean los metodos de POST PUT Y DELETE de la coleccion de insumos
    
    by: Jhon Hernandez

---
+++ versiones/V2/insumos.js
@@ -0,0 +1,53 @@
+import { db } from "../../config/variables.js";
+import { autoIncrement } from "../../helpers/autoincrement.js";
+
+let insumos = await db.collection("insumos");
+
+export const insumosPost = async (req, res) => {
+    try {
+        let newID = await autoIncrement("insumos")
+
+        let data = await insumos.insertOne({
+            id: newID,
+            ...req.body
+        })
+            
+        res.status(200).send({status: 200, message: "Registro creado con exito"});
+
+    } catch (error) {
+      res.status(400).send({ status: 400, message: error });
+    }
+};
+
+export const insumosPut = async (req, res) => {
+    try {
+        let id = req.params.id
+        id = parseInt(id)
+
+        let data = await insumos.updateOne(
+            { id: id },
+            { $set: req.body }
+        )
+            
+        res.status(200).send({status: 200, message: "Registro actualizado con exito"});
+
+    } catch (error) {
+      res.status(400).send({ status: 400, message: error });
+    }
+};
+
+export const insumosDelete = async (req, res) => {
+    try {
+        let id = req.params.id
+        id = parseInt(id)
+
+        let data = await insumos.deleteOne(
+            { id: id },
+        )
+            
+        res.status(200).send({status: 200, message: "Registro eliminado con exito"});
+
+    } catch (error) {
+      res.status(400).send({ status: 400, message: error });
+    }
+};
