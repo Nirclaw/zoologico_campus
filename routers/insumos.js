4e9c669008d30b05422afa7228c787d32efadba5
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-08-29 21:58:52 -0500

    feat: :sparkles: Routers
    Se crean los routes de los metodos get post put y delete con sus respectivas versiones
    
    by: Jhon Hernandez

--- routers/insumos.js
+++ routers/insumos.js
@@ -1,8 +1,40 @@
 import { Router } from "express";
 import passport from "../jwt/passport.js";
 import { limite } from "../middleware/limit.js";
+import { version } from "../config/variables.js";
+
+import { insumosAll, insumosName } from "../versiones/V1/insumos.js";
+import { insumosPost, insumosPut, insumosDelete } from "../versiones/V2/insumos.js";
+
 const appInsumos = Router();
 
 appInsumos.use(limite(),passport.authenticate("bearer", { session: false }));
+
+appInsumos.get("/", version({
+        "1.0.0": insumosAll,
+        "2.0.0": insumosAll,
+    })
+);
+
+appInsumos.get("/nombre/:nombre", version({
+        "1.0.0": insumosName,
+        "2.0.0": insumosName,
+    })
+);
+
+appInsumos.post("/", version({
+        "2.0.0": insumosPost,
+    })
+);
+
+appInsumos.put("/:id", version({
+        "2.0.0": insumosPut,
+    })
+);
+
+appInsumos.delete("/:id", version({
+        "2.0.0": insumosDelete,
+    })
+);
 
 export default appInsumos;
